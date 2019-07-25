f9e18fcd189165486de68142df75c09637d50a5d
Cheng Zhang <showt2me@gmail.com>
2019-07-25 23:29:26 +0800

    [Improvement] Polish demo project

--- Example/SwiftWebImageDemo/SwiftWebImageDemo/ContentView.swift
+++ Example/SwiftWebImageDemo/SwiftWebImageDemo/ContentView.swift
@@ -13,28 +13,18 @@ struct Feed: Identifiable {
     let id: UUID = UUID()
     let imageUrl: String
     
-    static let list: [Feed] = [
-        .init(imageUrl: "https://d37t5b145o82az.cloudfront.net/pictures/01bff78eae0870a01ed491ef86405bdf.jpg"),
-        .init(imageUrl: "https://d37t5b145o82az.cloudfront.net/pictures/14729eb660b3a409368f820a053ac319.jpg"),
-        .init(imageUrl: "https://d37t5b145o82az.cloudfront.net/pictures/16c9316d8f5dbccf394f20361c96a541.jpg"),
-        .init(imageUrl: "https://d37t5b145o82az.cloudfront.net/pictures/297ee57338cb757d5bf359f5f0dd666f.jpg"),
-        .init(imageUrl: "https://d37t5b145o82az.cloudfront.net/pictures/3a7635518ee11c02c113c6cb88f1613e.jpg")
-    ]
+    static let sample = Feed(imageUrl: "https://d37t5b145o82az.cloudfront.net/pictures/01bff78eae0870a01ed491ef86405bdf.jpg")
 }
 
 struct ContentView : View {
     
     var body: some View {
-        List {
-            ForEach(Feed.list.identified(by: \.id)) { feed in
-                SwiftImage(imageUrl: feed.imageUrl) { imageView in
-                    AnyView(
-                        imageView
-                            .resizable()
-                            .aspectRatio(1, contentMode: .fit)
-                    )
-                }
-            }
+        SwiftImage(imageUrl: Feed.sample.imageUrl) { imageView in
+            AnyView(
+                imageView
+                    .resizable()
+                    .aspectRatio(1, contentMode: .fit)
+            )
         }
     }
     
