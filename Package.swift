ae2ad2e5054322b348a42dae7d7419efad079572
Cheng Zhang <showt2me@gmail.com>
2020-04-15 00:06:20 +0900

    [Project] Convert to dynamic library.

--- Package.swift
+++ Package.swift
@@ -12,6 +12,7 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "SwiftWebImage",
+            type: .dynamic,
             targets: ["SwiftWebImage"]),
     ],
     dependencies: [
