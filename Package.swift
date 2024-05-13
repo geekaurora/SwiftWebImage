df43ced77a41c869e7fe69fc8fd14583afd6ba5a
cheng zhang <showt2me@gmail.com>
2024-05-12 19:15:34 -0700

    Update minimums support iOS to v15.

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "SwiftWebImage",
     platforms: [
-        .iOS(.v14),
+      .iOS("15.0")
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
