bd3251b4de3114ee8f64651bff2b1b33b1352d7c
cheng zhang <showt2me@gmail.com>
2025-02-22 18:12:31 -0800

    Update demo image urls.

--- Example/SwiftWebImageDemo/ContentView.swift
+++ Example/SwiftWebImageDemo/ContentView.swift
@@ -6,16 +6,15 @@ struct Feed: Identifiable {
   let imageUrl: String
   
   static let list: [Feed] = [
-    .init(imageUrl: "https://d37t5b145o82az.cloudfront.net/pictures/01bff78eae0870a01ed491ef86405bdf.jpg"),
-    .init(imageUrl: "https://d37t5b145o82az.cloudfront.net/pictures/14729eb660b3a409368f820a053ac319.jpg"),
-    .init(imageUrl: "https://d37t5b145o82az.cloudfront.net/pictures/16c9316d8f5dbccf394f20361c96a541.jpg"),
-    .init(imageUrl: "https://d37t5b145o82az.cloudfront.net/pictures/297ee57338cb757d5bf359f5f0dd666f.jpg"),
-    .init(imageUrl: "https://d37t5b145o82az.cloudfront.net/pictures/3a7635518ee11c02c113c6cb88f1613e.jpg")
+    .init(imageUrl: "https://raw.githubusercontent.com/geekaurora/resources/main/images/01bff78eae0870a01ed491ef86405bdf.jpg"),
+    .init(imageUrl: "https://raw.githubusercontent.com/geekaurora/resources/main/images/14729eb660b3a409368f820a053ac319.jpg"),
+    .init(imageUrl: "https://raw.githubusercontent.com/geekaurora/resources/main/images/16c9316d8f5dbccf394f20361c96a541.jpg"),
+    .init(imageUrl: "https://raw.githubusercontent.com/geekaurora/resources/main/images/297ee57338cb757d5bf359f5f0dd666f.jpg"),
+    .init(imageUrl: "https://raw.githubusercontent.com/geekaurora/resources/main/images/3a7635518ee11c02c113c6cb88f1613e.jpg")
   ]
 }
 
 struct ContentView : View {
-  
   var body: some View {
     List {
       ForEach(Feed.list) { feed in
@@ -29,10 +28,3 @@ struct ContentView : View {
   }
 }
 
-#if DEBUG
-struct ContentView_Previews : PreviewProvider {
-  static var previews: some View {
-    ContentView()
-  }
-}
-#endif
